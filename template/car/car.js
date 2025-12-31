d8260e4863d1ecb3cee881402bc255de08d3dd1d
arco14 <osvaldo.arco0794@gmail.com>
2025-12-31 03:30:14 -0600

    obteniendo la informacion de los productos del carrito y del empleado

--- template/car/car.js
+++ template/car/car.js
@@ -1,18 +1,9 @@
 window.addEventListener('DOMContentLoaded', async () => {
     const url = CONFIG.API_URL
-
-    //? obtener firma por gafete
-    const firma =  14130589
-
-    const jsonEmpleado = {
-        Stored: 'dbo.PA_ComedorG3T',
-        Opcion: 'CE',
-        Firma: firma
-    }
-    return
-    const resData = await loadAPI(`${url}COMEDOR`, 'POST', jsonEmpleado, '', false)
-    console.log(resData)
-    const infoEmpleado = resData.response[0][0]
-    $('#numEmpleado').html(infoEmpleado.NUMERO_EMPLEADO)
-    $('#infoEmpleado').html(`${infoEmpleado.NOMBRE}, ${infoEmpleado.DEPARTAMENTO}`)
+    $('#btnGuardarConsumo').click(() => {
+        const carrito = JSON.parse(document.getElementById('arrayCarrito').value)
+        console.log(carrito)
+        const infoEmpleado = JSON.parse(document.getElementById('arrayEmpleado').value)
+        console.log(infoEmpleado)
+    })
 })
--- template/car/car.php
+++ template/car/car.php
@@ -1,8 +1,9 @@
+
 <div class="car">
     <div class="p-3 border-bottom rounded">
         <div class="d-flex align-items-center gap-2">
             <i class="fas fa-user iconEmpleado"></i>
-            <p id="numEmpleado" class="mb-0 h3" style="color: #e89c00;">
+            <p id="firmaEmpleado" class="mb-0 h3" style="color: #e89c00;">
                 <div id="infoEmpleado" class="h4 mb-0"></div>
             </p>
         </div>
@@ -13,9 +14,11 @@
             <p class="h4 text-uppercase mb-0" style="color: #e89c00;">Total Productos: <span id="total" style="color: #6B7280;">0</span></p>
             <p class="h4 text-uppercase mb-0" style="color: #e89c00;">Precio: <span id="precio" style="color: #6B7280;">$ 0</span></p>
         </div>
-        <div class="btn btnConsumo text-uppercase"> 
+        <div id="btnGuardarConsumo" class="btn btnConsumo text-uppercase"> 
             Guardar
         </div>
     </div>
 </div>
-<script src="../../../../comedor/template/car/car.js"></script>
+<input type="hidden" id="arrayCarrito">
+<input type="hidden" id="arrayEmpleado">
+<script type="module" src="../../../../comedor/template/car/car.js"></script>

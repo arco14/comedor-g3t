bb6ec2ff3fe693d80534c9c8e898387442aba8c7
arco14 <osvaldo.arco0794@gmail.com>
2026-01-05 16:03:43 -0600

    Correccion bug de firma vacia

--- template/template.php
+++ template/template.php
@@ -3,12 +3,12 @@
     // include_once './template/banner/banner.php';
 ?>
     <div class="row h-100 border">
-        <div class="col-lg-9 p-5">
+        <div class="col-lg-8 p-5">
             <?php 
                 include_once './template/content/content.php';
             ?>  
         </div>
-        <div class="col-lg-3">
+        <div class="col-lg-4">
             <?php 
                 include_once './template/car/car.php';
             ?> 
